23435daabcd9e293e6ef0f1304cf7209600ab070
ifpen-gp <63413841+ifpen-gp@users.noreply.github.com>
2026-03-26 17:12:27 +0100

    added: pytest ignores directory tests/ignored

---
+++ tests/ignored/test_ignored.py
@@ -0,0 +1,10 @@
+#!python3
+# -*- coding: utf-8 -*-
+"""
+This test is ignored when launching pytest command because located in tests/ignored (as configured in tox.ini).
+"""
+from quickstart import hello_world
+
+
+def test_hello_world():
+    assert hello_world() is None
